efd6b496cf29c6dfa661257f2cf8048c8574204c
Victor Borneo <victorbor2001@yahoo.com.br>
2025-12-01 03:06:15 -0300

    added media and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# CUBE
+
+A simple 3D cube written JavaScript's 2d canvas.
+It only rotates, you can't turn the sides.
+
+![alt tag](./media/screenshot.png)
+![alt tag](./media/gif.gif)
--- consts.js
+++ consts.js
@@ -7,6 +7,6 @@ export const backgroundColor = 'grey';
 export const outlineColor = 'black';
 export const outlineWidth = 1;
 
-export const cubeN = 4;
+export const cubeN = 11;
 
 export const sensitivity = 0.01;
